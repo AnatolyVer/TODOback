894d0c65278c23022b1e1dd0bab713f91e5b9083
AnatolyVer <tolik.vereshchak88@gmail.com>
2023-07-17 16:11:56 +0300

    user dto changing

--- dto/userDto.js
+++ dto/userDto.js
@@ -4,12 +4,16 @@ class userDto{
     accessToken
     login
     tags
+    name
+    picture
     constructor(obj) {
         this.user_id = obj._id
         this.todos = obj.todos
         this.accessToken = obj.accessToken
         this.login = obj.login
         this.tags = obj.tags
+        this.name = obj.name
+        this.picture = obj.picture
     }
 }
 
