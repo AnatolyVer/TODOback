92ec68e06ff1d9872a7cb90eddc0d285dc1cd196
AnatolyVer <tolik.vereshchak88@gmail.com>
2023-09-08 18:00:33 +0300

    projectDto changes

--- dto/projectDto.js
+++ dto/projectDto.js
@@ -5,6 +5,7 @@ class projectDto{
     color
     isPinned
     todos
+    shared
     constructor(project) {
         this.id = project._id
         this.name = project.name
@@ -12,6 +13,8 @@ class projectDto{
         this.color = project.color
         this.isPinned = project.isPinned
         this.todos = project.todos
+        this.shared = project.shared
+
     }
 }
 
