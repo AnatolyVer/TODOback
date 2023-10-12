92297cef7c26604dd260197dc96e7824d8f062a8
AnatolyVer <tolik.vereshchak88@gmail.com>
2023-10-12 18:44:09 +0300

    WebSocket refactoring

--- WebSocket.js
+++ WebSocket.js
@@ -1,76 +1,91 @@
 import { WebSocketServer } from 'ws';
 
+const EVENT_CONNECTION = 'connection';
+const EVENT_MESSAGE = 'message';
+const EVENT_CLOSE = 'close';
+
 class WebSocketManager {
     constructor(server) {
-        this.wss = new WebSocketServer({server})
-        this.clients = {}
-        this.rooms = {}
+        this.wss = new WebSocketServer({ server });
+        this.clients = {};
+        this.rooms = {};
 
-        this.wss.on('connection', (socket, req) => {
-            console.log('Новое подключение к серверу сокетов');
-            const params = req.url.substr(1).split('&');
+        this.wss.on(EVENT_CONNECTION, (socket, req) => this.onConnection(socket, req));
+    }
 
-            const paramObj = {};
-            params.forEach(param => {
-                const [key, value] = param.split('=');
-                paramObj[key] = value;
-            });
+    onConnection(socket, req) {
+        console.log('Connection is opened');
+        const {id, room} = this.parseRequestParameters(req.url.substr(1));
 
-            this.clients[paramObj["id"]] = {
-                socket:socket,
-                room:paramObj["room"]
-            };
+        this.clients[id] = {
+            socket,
+            room
+        };
 
-            this.joinRoom(paramObj["id"], this.clients[paramObj["id"]].room)
+        this.addClientToRoom(id, room);
 
-            socket.on('message', (message) => {
-                const client = this.findClientBySocket(socket)
-                this.sendToRoom(client.room, message)
-            });
+        socket.on(EVENT_MESSAGE, message => {
+            this.sendMessageToRoom(socket, message);
+        });
 
-            socket.on('close', () => {
-                for (let key in this.clients) {
-                    if (this.clients.hasOwnProperty(key) && this.clients[key].socket === socket) {
-                        this.leaveRoom(key, this.clients[key].room)
-                        delete this.clients[key];
-                    }
-                }
-                console.log('Соединение закрыто');
-            });
+        socket.on(EVENT_CLOSE, () => {
+            this.closeSocket(socket)
         });
     }
 
-    joinRoom(clientId, roomName) {
-        if (this.rooms[roomName]) {
-            this.rooms[roomName].add(clientId);
-        }
-        else {
+    parseRequestParameters(url) {
+        const params = url.split('&');
+        const paramObj = {};
+
+        params.forEach(param => {
+            const [key, value] = param.split('=');
+            paramObj[key] = value;
+        });
+
+        return paramObj;
+    }
+
+    addClientToRoom(clientId, roomName) {
+        if (!this.rooms[roomName]) {
             this.rooms[roomName] = new Set();
-            this.rooms[roomName].add(clientId);
         }
+        this.rooms[roomName].add(clientId);
     }
-
-    leaveRoom(clientId, roomName) {
-        if (this.rooms[roomName]) {
-            this.rooms[roomName].delete(clientId);
-        }
-    }
-
-    findClientBySocket(socket){
+    findClientBySocket(socket) {
         for (let id in this.clients) {
             if (this.clients.hasOwnProperty(id) && this.clients[id].socket === socket) {
-                return this.clients[id]
+                return this.clients[id];
             }
         }
     }
-
-    sendToRoom(roomName, message) {
-        if (this.rooms[roomName]) {
-            this.rooms[roomName].forEach((clientId) => {
-                if (this.clients[clientId]) {
-                    this.clients[clientId].socket.send(message);
+    sendMessageToRoom(socket, message) {
+        const client = this.findClientBySocket(socket);
+        if (this.rooms[client.room]) {
+            this.rooms[client.room].forEach(clientId => {
+                const client = this.clients[clientId];
+                if (client) {
+                    client.socket.send(message);
                 }
             });
+        }
+    }
+    removeClientFromRoom(clientId, roomName) {
+        const room = this.rooms[roomName];
+        if (room) {
+            room.delete(clientId);
+            if (room.size === 0) {
+                delete this.rooms[roomName];
+            }
+        }
+    }
+    closeSocket(socket){
+        for (let key in this.clients) {
+            if (this.clients.hasOwnProperty(key) && this.clients[key].socket === socket) {
+                this.removeClientFromRoom(key, this.clients[key].room);
+                delete this.clients[key];
+                console.log('Connection is closed');
+                return
+            }
         }
     }
 }
